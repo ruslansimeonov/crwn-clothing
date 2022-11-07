2913a69c1eb522d17d218dc811146658b68218f8
Ruslan Simeonov <ruslansimenov@gmail.com>
2022-11-07 13:31:14 +0100

    Changed access to states using redux

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -3,14 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import {
   BackgroundImage,
   Body,
-  DirectoryItemContainer
+  DirectoryItemContainer,
 } from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title, route } = category;
   const navigate = useNavigate();
 
-  const onNavigateHandler = () => navigate(route)
+  const onNavigateHandler = () => navigate(route);
+
   return (
     <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
